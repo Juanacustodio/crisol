29336832ac38c4a269bf10a3b24865d71b352c8d
Juana Custodio <custodim.juana@gmail.com>
2020-06-28 15:55:38 -0500

    add login view

--- app/src/main/java/com/example/crisol/fragments/FavoriteFragment.java
+++ app/src/main/java/com/example/crisol/fragments/FavoriteFragment.java
@@ -1,14 +1,17 @@
 package com.example.crisol.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.crisol.LoginActivity;
 import com.example.crisol.R;
 
 public class FavoriteFragment extends Fragment {
@@ -16,6 +19,18 @@ public class FavoriteFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View root = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        // btnLogin
+        Button btnLogin = root.findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
+
+        return root;
     }
 }
