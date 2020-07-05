65e034e6cc434ebb67e6244f778395a0a3633931
Juana Custodio <custodim.juana@gmail.com>
2020-07-05 17:24:37 -0500

    book detail

--- app/src/main/java/com/example/crisol/BookDetailActivity.java
+++ app/src/main/java/com/example/crisol/BookDetailActivity.java
@@ -3,6 +3,12 @@ package com.example.crisol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.crisol.model.Book;
+import com.squareup.picasso.Picasso;
 
 public class BookDetailActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class BookDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+
+        ImageView ivImage = findViewById(R.id.ivImage);
+        TextView tvTitle = findViewById(R.id.tvTitle);
+        TextView tvAuthor = findViewById(R.id.tvAuthor);
+        Button btnBuy = findViewById(R.id.btnBuy);
+        TextView tvSummary = findViewById(R.id.tvSummary);
+
+        if(getIntent().hasExtra("book")){
+            Book book = getIntent().getParcelableExtra("book");
+            // Utilizando la librería Picasso
+            Picasso.get().load(book.getImage()).into(ivImage);
+            tvTitle.setText(book.getTitle());
+            tvAuthor.setText(book.getAuthorId());
+            btnBuy.setText("Comprar por " + book.getPrice());
+            tvSummary.setText(book.getSummary());
+        }
     }
 }
