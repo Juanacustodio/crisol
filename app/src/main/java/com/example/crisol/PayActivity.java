8714929407c5bc7ef4365c82b0c78596bb7e0359
Juana Custodio <custodim.juana@gmail.com>
2020-07-09 22:19:34 -0500

    start pay activity

--- app/src/main/java/com/example/crisol/PayActivity.java
+++ app/src/main/java/com/example/crisol/PayActivity.java
@@ -12,18 +12,16 @@ import android.widget.TextView;
 import com.example.crisol.model.Book;
 import com.squareup.picasso.Picasso;
 
-public class BookDetailActivity extends AppCompatActivity {
+public class PayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_detail);
+        setContentView(R.layout.activity_pay);
 
         ImageView ivImage = findViewById(R.id.ivImage);
         TextView tvTitle = findViewById(R.id.tvTitle);
         TextView tvAuthor = findViewById(R.id.tvAuthor);
-        Button btnBuy = findViewById(R.id.btnBuy);
-        TextView tvSummary = findViewById(R.id.tvSummary);
 
         if(getIntent().hasExtra("book")){
             final Book book = getIntent().getParcelableExtra("book");
@@ -31,17 +29,6 @@ public class BookDetailActivity extends AppCompatActivity {
             Picasso.get().load(book.getImage()).into(ivImage);
             tvTitle.setText(book.getTitle());
             tvAuthor.setText(book.getAuthorId());
-            btnBuy.setText("Comprar por " + book.getPrice());
-            tvSummary.setText(book.getSummary());
-
-            btnBuy.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intPay = new Intent(BookDetailActivity.this, PayActivity.class);
-                    intPay.putExtra("book", book);
-                    startActivity(intPay);
-                }
-            });
         }
     }
 }
